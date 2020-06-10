02560612c18117e8427c35e493ab01d55178df7d
retnikt <master@retnikt.uk>
2020-06-10 09:39:51 +0100

    formatting

--- src/flake8/options/pyproject.py
+++ src/flake8/options/pyproject.py
@@ -1,7 +1,7 @@
-import toml
+"""Module to enable loading configuration from pyproject.toml files."""
+
 import logging
 import os.path
-
 
 LOG = logging.getLogger(__name__)
 
@@ -10,6 +10,13 @@ MAX_RECURSION = 25
 
 
 def parse_py_project_toml():
+    """Attempt to find and load configuration from a pyproject.toml file."""
+    try:
+        import toml
+    except ImportError:
+        LOG.debug("toml module not found, ignoring any pyproject.toml files")
+        return {}
+
     path = "pyproject.toml"
     for _ in range(MAX_RECURSION):
         try:
@@ -19,13 +26,23 @@ def parse_py_project_toml():
                     loaded = toml.load(f)
                     break
                 except toml.TomlDecodeError as e:
-                    LOG.error("failed to parse pyproject.toml at %s" % os.path.abspath(path), exc_info=e)
+                    LOG.error(
+                        "failed to parse pyproject.toml at %s"
+                        % os.path.abspath(path),
+                        exc_info=e,
+                    )
         except FileNotFoundError:
             path = "../" + path
         except OSError as e:
-            LOG.error("failed to open pyproject.toml at %s: %s" % (os.path.abspath(path), e), exc_info=e)
+            LOG.error(
+                "failed to open pyproject.toml at %s: %s"
+                % (os.path.abspath(path), e),
+                exc_info=e,
+            )
             path = "../" + path
     else:
         LOG.debug("no pyproject.toml found")
         return {}
-    return {k.replace("-", "_"): v for k, v in loaded["tool"]["flake8"].items()}
+    return {
+        k.replace("-", "_"): v for k, v in loaded["tool"]["flake8"].items()
+    }
